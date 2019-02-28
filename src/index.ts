b3e45af9cd0a776814478be36c14f519939b11cc
inkcAI <dev@inkc.io>
2019-02-28 10:25:11 +0800

    import bitcoin rpc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+import rpc from 'bitcoin-rpc-promise';
+
+async function run() {
+    let bitcoind = new rpc('http://alice:alice@10.168.3.131:8332');
+    let result = await bitcoind.call('getblockcount');
+    console.log(result);
+}
+
+run();
