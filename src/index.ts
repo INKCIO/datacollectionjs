33fc1dedfea2b4d90f9dcdd6f5b80f05fc79d018
inkcAI <dev@inkc.io>
2019-03-02 15:38:40 +0800

    get raw tx

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,19 @@ import rpc from 'bitcoin-rpc-promise';
 
 async function run() {
     let bitcoind = new rpc('http://alice:alice@10.168.3.131:8332');
-    let result = await bitcoind.call('getblockcount');
-    console.log(result);
+    let height = await bitcoind.call('getblockcount');
+    console.log(height);
+
+    let hash = await bitcoind.call('getblockhash', height);
+    console.log(hash);
+
+    let block = await bitcoind.call('getblock', hash);
+    let txid = block.tx[0];
+    console.log(block);
+    console.log(txid);
+
+    let tx = await bitcoind.call('getrawtransaction', txid);
+    console.log(tx);
 }
 
 run();
